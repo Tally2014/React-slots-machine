02087f6c05abf78badd7140fc090a7a3948b6f06
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-01-04 17:56:40 +0200

    Create a button to reload and function to reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css';
 import SlotsRoll from "./SlotsRoll"; 
+import React, { useState } from 'react';
 
 function App() {
-
+  const [reload, setReload] = useState(false);
+  const handleClick = () => {
+    setReload(!reload); // Toggle the state to trigger a re-render
+  }
   return (
     <>
-      <SlotsRoll/>
-      <SlotsRoll/>
-      <SlotsRoll/>
+      <SlotsRoll key={reload ? '1' : '0'}/>
+      <SlotsRoll key={reload ? '2' : '1'}/>
+      <SlotsRoll key={reload ? '3' : '2'}/>
+      <button onClick={handleClick}>Gamble</button>
     </>
   )
 }
