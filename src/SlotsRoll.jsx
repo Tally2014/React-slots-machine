be0284c6b056bddc0d35712cece137de8a28e0dc
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-01-04 17:32:52 +0200

    Randomizing Fruits Logic

--- src/SlotsRoll.jsx
+++ src/SlotsRoll.jsx
@@ -1,10 +1,13 @@
 import Slots from './Slots';
 
 export default function SlotsRoll() {
+    const slotsItems =['🍎','🍐','🍊','🍋','🍌','🍆','🍒','🍑']
+    function randomize(){
+        return Math.floor(Math.random() * slotsItems.length)
+    }
   return (
     <div>
-        <Slots val1="🍒" val2="🍒" val3="🍒"/>
-        <Slots val1="🍒" val2="🍌" val3="🍒"/>
+        <Slots val1={slotsItems[randomize()]} val2={slotsItems[randomize()]} val3={slotsItems[randomize()]}/>
     </div>
   )
 }
