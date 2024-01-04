f523daf6c1a634c050f645125b8520ec751fa47b
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-01-04 16:44:35 +0200

    Completed my solution

--- src/Slots.jsx
+++ src/Slots.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 function Slots({val1, val2, val3}) {
+    const isMatched = val1 === val2 && val1 === val3;
+    const styles = {color: isMatched? "green": "red"};
   return (
     <div>
         <h1>{val1} {val2} {val3}</h1>
-        <h2>You win!</h2>
-        <h3>Congrats!!!</h3>
+        {isMatched? <h2 style={styles}>You win!</h2>: <h2 style={styles}>You lose</h2>}
+        {isMatched && <h3>Congrats!!!</h3>}
     </div>
   )
 }
